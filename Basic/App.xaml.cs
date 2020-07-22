187e2cb1fe8dfbaf819e2a30df17817f79e108a1
Graeme Mulvaney <web.bod@gmail.com>
2020-07-22 10:12:12 +0100

    Configure a default dictionary, loading a different one if needed

--- Basic/App.xaml.cs
+++ Basic/App.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
@@ -13,5 +14,30 @@ namespace Basic
     /// </summary>
     public partial class App : Application
     {
+        public App()
+        {
+            InitializeComponent();
+            LoadCorrectDictionary();
+        }
+
+        protected void LoadCorrectDictionary(string dictionaryName = "deDE")
+        {
+            try
+            {
+                var dictionary = new ResourceDictionary();
+
+                // replace the default dictionary with the german one
+                var path = Path.Combine("Resources", "Dictionaries", $"{dictionaryName}.xaml");
+                dictionary.Source = new Uri(path, UriKind.Relative);
+
+
+                // this feels a wee bit britle
+                Resources.MergedDictionaries[0] = dictionary;
+            }
+            catch (Exception ex)
+            {
+                // happend when the dictionary is not found
+            }
+        }
     }
 }
